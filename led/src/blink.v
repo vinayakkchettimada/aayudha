9c8d19e2f02274aad0cef98c6c84b2e95b6a2672
Vinayak Kariappa Chettimada <vinayak.kariappa@gmail.com>
2014-11-16 14:22:01 +0100

    Initial commit. A working blinking LED and LED ON on rst press.

---
+++ led/src/blink.v
@@ -0,0 +1,23 @@
+module blink (
+    input clk,
+    input rst,
+    output out
+    );
+ 
+reg [24:0] counter_d, counter_q;
+ 
+assign out = counter_q[24];
+ 
+always @(counter_q) begin
+    counter_d = counter_q + 1'b1;
+end
+ 
+always @(posedge clk) begin
+    if (rst) begin
+        counter_q <= 25'b0;
+    end else begin
+        counter_q <= counter_d;
+    end
+end
+ 
+endmodule
